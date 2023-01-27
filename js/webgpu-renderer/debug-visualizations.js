ddfc219711739688277deb5c3af5490de919af47
Brandon Jones <bajones@chromium.org>
2023-01-27 11:06:27 -0800

    Fix outdated shader syntax

--- js/webgpu-renderer/debug-visualizations.js
+++ js/webgpu-renderer/debug-visualizations.js
@@ -101,7 +101,7 @@ export class ClusterDistanceVisualization extends RenderBundleHelper {
     ${TileFunctions}
 
     ${ClusterStructs}
-    @group(3) @binding(0) var<storage_buffer, read> clusters : Clusters;
+    @group(3) @binding(0) var<storage, read> clusters : Clusters;
 
     struct FragmentInput {
       @builtin(position) fragCoord : vec4<f32>,
